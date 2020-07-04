440132402e7f40204c42da1b5a93840e747f8c5d
Sabrina Snider <sabrina.snider10@gmail.com>
2020-07-04 19:12:40 -0400

    Loaded database product names into page.

--- client/src/views/StoreList.vue
+++ client/src/views/StoreList.vue
@@ -2,14 +2,33 @@
   <div id="list-container" class="page">
     <h2 id="store-name">{{$route.params.storeName | capitalize}}</h2>
     <div id="list">
-      item
+      <Checkbox v-for="product in data" :key="product.id" v-bind:product="product"></Checkbox>
     </div>
   </div>
 </template>
 
 <script>
+import Checkbox from '../components/Checkbox';
+import axios from 'axios';
+
 export default {
-  name: "StoreList"
+  name: "StoreList",
+  components: {
+    Checkbox
+  },
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:5000/all')
+    .then(
+      response => (
+        this.data = response.data
+      )
+    )
+  }
 }
 </script>
 
@@ -28,9 +47,8 @@ export default {
 
 #list {
   background-color: white;
-  border: 2px solid $gray;
-  border-radius: 5px;
+  border: 1px solid $gray;
+  border-radius: 3px;
   padding: 20px;
 }
-
 </style>
