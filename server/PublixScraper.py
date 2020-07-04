96d92146ccd0ee96335bc9c3e57e786192018bca
Sabrina Snider <sabrina.snider10@gmail.com>
2020-07-04 01:58:15 -0400

    Got a basic web scraper working for Publix in the  jupyter notebook. The current store location is fixed.

---
+++ server/PublixScraper.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getPublixProductID(itemName):
+  url = "https://services.publix.com/api/v3/product/Search?storeNumber=537&keyword=" + itemName
+  api_response = requests.get(url).json()
+  
+  if(len(api_response["Products"]) == 0):
+    return None
+  
+  return api_response["Products"][0]["Productid"]
+
+def getItemLocation(itemName):
+  # Store location for the search
+  STORE_COOKIE = "%7B%22StoreName%22%3A%22Butler%20Plaza%20West%22%2C%22StoreNumber%22%3A1312%2C%22Option%22%3A%22ACDFJNORU%22%2C%22ShortStoreName%22%3A%22Butler%20Plaza%20West%22%7D"
+  
+  # Get product ID from Publix
+  productID = getPublixProductID(itemName)
+  url = "http://publix.com/pd/" + productID
+  
+  # Set location by sending a cookie through a session.
+  session = requests.Session()
+  session.post(url, cookies = {'Store': STORE_COOKIE})
+  
+  # Make get request for product page information.
+  request = session.get(url)
+  page = BeautifulSoup(request.content, 'html.parser')
+  
+  # Extract location from page.
+  locationTags = page.findAll("li", {"class" : "location"})
+  
+  return locationTags[0].find("span").get_text()
