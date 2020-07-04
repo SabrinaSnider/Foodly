d9b5a8abf4f4a52b4058c2a13ae42c93efb4aa87
Sabrina Snider <sabrina.snider10@gmail.com>
2020-07-04 10:44:03 -0400

    Added item location get request to server.

--- server/PublixScraper.py
+++ server/PublixScraper.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+STORE_COOKIE = "%7B%22StoreName%22%3A%22Butler%20Plaza%20West%22%2C%22StoreNumber%22%3A1312%2C%22Option%22%3A%22ACDFJNORU%22%2C%22ShortStoreName%22%3A%22Butler%20Plaza%20West%22%7D"
+
 def getPublixProductID(itemName):
   url = "https://services.publix.com/api/v3/product/Search?storeNumber=537&keyword=" + itemName
   api_response = requests.get(url).json()
@@ -11,11 +13,11 @@ def getPublixProductID(itemName):
   return api_response["Products"][0]["Productid"]
 
 def getItemLocation(itemName):
-  # Store location for the search
-  STORE_COOKIE = "%7B%22StoreName%22%3A%22Butler%20Plaza%20West%22%2C%22StoreNumber%22%3A1312%2C%22Option%22%3A%22ACDFJNORU%22%2C%22ShortStoreName%22%3A%22Butler%20Plaza%20West%22%7D"
-  
   # Get product ID from Publix
   productID = getPublixProductID(itemName)
+  if productID is None:
+    return "unknown"
+
   url = "http://publix.com/pd/" + productID
   
   # Set location by sending a cookie through a session.
@@ -29,4 +31,7 @@ def getItemLocation(itemName):
   # Extract location from page.
   locationTags = page.findAll("li", {"class" : "location"})
   
+  if len(locationTags) == 0: 
+    return "unknown"
+    
   return locationTags[0].find("span").get_text()
--- server/app.py
+++ server/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+# To start server:
+# $env:FLASK_APP = "app.py"
+# $env:FLASK_ENV = "development"
+# flask run
+
 # configuration
 DEBUG = True
 
@@ -11,11 +16,16 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-# sanity check route
+# Routes
+from PublixScraper import getItemLocation
+
 @app.route('/ping', methods=['GET'])
 def ping_pong():
-    return jsonify('pong!')
+  return jsonify('pong!')
 
+@app.route('/item/<string:item>')
+def itemLocation(item):
+  return getItemLocation(item)
 
 if __name__ == '__main__':
     app.run()
